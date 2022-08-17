348673b5c91310fd3a4f08f631d6b8f57ee98235
Taohit Abdul <taohit.abdul@wbaoneit.com>
2022-08-17 14:34:10 +0100

    Validation for student id

--- logic/http.js
+++ logic/http.js
@@ -8,4 +8,22 @@ export async function getStudentStatusFromMocki(studentId) {
   // Request type: GET
   // valid ID: 1bb839fb-4ed8-4855-ab2c-bb535e0fef96
   // WRITE YOUR CODE HERE
+
+  { 
+    let studentId = [
+       {"id":number,"firstName": string,"lastName":string,"email":email,"schoolEndDate":date,"schoolName":string}
+    ]
+   }
+
+  const response = await fetch('https://mocki.io/v1/1bb839fb-4ed8-4855-ab2c-bb535e0fef96')
+
+  if (!response.ok) return false;
+
+  const data = await response.json()
+  
+
+  const dataExists = Boolean(data);
+
+  return dataExists;
+
 }
