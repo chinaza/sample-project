7fa705ac4dee6a27ec3479c1fa111ba6b85a766e
TaohitA <111287783+TaohitA@users.noreply.github.com>
2022-08-19 00:47:04 +0100

    Update https
    Removed studentId object.

--- logic/http.js
+++ logic/http.js
@@ -9,12 +9,6 @@ export async function getStudentStatusFromMocki(studentId) {
   // valid ID: 1bb839fb-4ed8-4855-ab2c-bb535e0fef96
   // WRITE YOUR CODE HERE
 
-  { 
-    let studentId = [
-       {"id":number,"firstName": string,"lastName":string,"email":email,"schoolEndDate":date,"schoolName":string}
-    ]
-   }
-
   const response = await fetch('https://mocki.io/v1/1bb839fb-4ed8-4855-ab2c-bb535e0fef96')
 
   if (!response.ok) return false;
