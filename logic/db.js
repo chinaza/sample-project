13f98a2cec4871c17adb618ece18e76931603b6f
Taohit Abdul <taohit.abdul@wbaoneit.com>
2022-08-23 11:56:02 +0100

    Save data

--- logic/db.js
+++ logic/db.js
@@ -10,8 +10,3 @@ export async function saveToDB(student) {
   await ValidationModel.create(student);
 }
 
-/**
- * @param  {string} id
- * @returns {Promise<{studentId: string, name: string, isStudent: boolean}>}
- */
-
