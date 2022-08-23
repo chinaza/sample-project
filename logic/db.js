98c4c459d8b64be7cc4d3e93498512e48c8e40a8
ruth <ruth.abeam@wbaoneit.com>
2022-08-23 12:05:16 +0100

    #2 retrieve data

--- logic/db.js
+++ logic/db.js
@@ -1,25 +1,17 @@
-import ValidationModel from '../models/validations';
 
-/**
- * @param  {{studentId: string, name: string, isStudent: boolean}} student
- * @returns {Promise<void>}
- */
-export async function saveToDB(student) {
-  // WRITE YOUR CODE HERE
-}
+    
+    
+    
+
 
 /**
  * @param  {string} id
  * @returns {Promise<{studentId: string, name: string, isStudent: boolean}>}
  */
 export async function getFromDB(id) {
-  // WRITE YOUR CODE HERE
-}
-export async function saveToDB(student) {
-  export async function getFromDB(id) {
-    const result = await ValidationModel.findOne({studentId: id});
-    return result;
-  }
-}
+  const result = await ValidationModel.findOne({studentId: id});
+        return result;
+      }
+    
 
-
+ 
