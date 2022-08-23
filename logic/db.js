5ba3328d1bf97a9aac74e362900a9693e269cad1
Taohit Abdul <taohit.abdul@wbaoneit.com>
2022-08-23 11:25:49 +0100

    Saved data tomongodb

--- logic/db.js
+++ logic/db.js
@@ -4,9 +4,7 @@ import ValidationModel from '../models/validations';
  * @param  {{studentId: string, name: string, isStudent: boolean}} student
  * @returns {Promise<void>}
  */
-export async function saveToDB(student) {
-  // WRITE YOUR CODE HERE
-}
+
 export async function saveToDB(student) {
   
   await ValidationModel.create(student);
@@ -16,6 +14,4 @@ export async function saveToDB(student) {
  * @param  {string} id
  * @returns {Promise<{studentId: string, name: string, isStudent: boolean}>}
  */
-export async function getFromDB(id) {
-  // WRITE YOUR CODE HERE
-}
+
